0e0a6ab1ee492bb72b0b2ca14a554ad6253b4628
zblongfei <zblongfei@gmail.com>
2018-03-27 20:41:58 +0800

    add babel

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -25,7 +25,7 @@ module.exports = {
     rules: [
       {
         test: /\.(tsx|ts)$/,
-        use: ['ts-loader'],
+        use: ['babel-loader', 'ts-loader'],
         exclude: ['node_modules']
       },
       {
