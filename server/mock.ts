a896b12c4d7938cfa2f7861c83ce804e5a1c1ef5
zblongfei <zblongfei@gmail.com>
2018-04-03 22:35:40 +0800

    update

--- server/mock.ts
+++ server/mock.ts
@@ -8,7 +8,7 @@ export default (app: express.Express) => {
         code: 200,
         data: [
           { id: 1, isDone: true, value: 'one' },
-          { id: 2, isDone: false, value: 'todo' }
+          { id: 2, isDone: false, value: 'todos' }
         ]
       })
     )
